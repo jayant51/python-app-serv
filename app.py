cd8c0e35d573a9af877e601f40a0dc4be58a732e
jayantkulkarni <jayant.kulkarni@ibm.com>
2022-04-14 23:05:27 -0700

    Added API

--- app.py
+++ app.py
@@ -1,21 +1,11 @@
-from flask import Flask, jsonify, request
+from flask import Flask
 from waitress import serve
-
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return ('message: Remote VM execution')
-
-
-@app.route('/restartvm')
-def restartVM():
-    #sshclnt = utils.getSSHClient()
-	#s = sshclnt.get_transport().open_session()
-	#paramiko.agent.AgentRequestHandler(s)
-	#sshclnt.exec_command("sudo /sbin/reboot", get_pty=True)
-	return ('message: Sent Restart remote server : services-uscentral.skytap.com')
+    return "Message : Remote VM Execution !"
 
 if __name__ == '__main__':
     serve(app, host='0.0.0.0', port=8080)
