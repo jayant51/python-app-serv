ca33071f54da43777983c806b9ff25da1ca20b02
jayantkulkarni <jayant.kulkarni@ibm.com>
2022-04-14 22:45:46 -0700

    Added API

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 from waitress import serve
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello World!"
+    return jsonify({'message': 'Remote VM execution'})
 
 if __name__ == '__main__':
     serve(app, host='0.0.0.0', port=8080)
